458bc9f76d3cdb6c847cd50f6d734ec9f82de3b5
asimsial <imabdulrehman94@gmail.com>
2025-05-23 19:35:28 +0500

    covert code into oops 'App' class

--- gui/main.py
+++ gui/main.py
@@ -1,8 +1,15 @@
 import ttkbootstrap as ttk
+from ttkbootstrap.constants import *
 
-root = ttk.Window(themename="superhero")
+class App:
+    def __init__(self):
+        self.root = ttk.Window(themename="superhero")
+        self.root.title("POS System")
+        self.root.geometry("1000x600")
+        
+    def run(self):
+        self.root.mainloop()
 
-root.title("POS System")
-root.geometry("1000x600")
-
-root.mainloop()
+if __name__ == "__main__":
+    app = App()
+    app.run()
